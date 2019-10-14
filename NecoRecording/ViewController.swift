a672ff83a238abf675da009a531ba29da08a84fa
Fernando Ortiz <ortizfernandomartin@gmail.com>
2019-10-14 19:48:48 -0300

    Initial Commit

---
+++ NecoRecording/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NecoRecording
+//
+//  Created by Fernando Martín Ortiz on 14/10/2019.
+//  Copyright © 2019 Fernando Martín Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
